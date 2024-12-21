01248fc660f6fccb2332bc06cda698b083e8e102
michael philip-apoe <onomeapoe@gmail.com>
2024-12-21 11:15:47 +0100

    edit cors

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://multi-vendor-ecommerce-app-5.onrender.com/api/v2";
+export const server = "https://multi-vendor-ecommerce-app-7.onrender.com/api/v2";
 
-export const backend_url="https://multi-vendor-ecommerce-app-5.onrender.com"
+export const backend_url="https://multi-vendor-ecommerce-app-7.onrender.com"
